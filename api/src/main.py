905e3d874af72ebab4418eeee16e9ec31a541eb8
Marcelo Santos <marcelo.net77@hotmail.com>
2026-02-22 19:31:57 +0000

    test: runned frontend worked ok

--- api/src/main.py
+++ api/src/main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI, Depends, HTTPException, Header
 import ollama
 import os
 from dotenv import load_dotenv
+from fastapi.responses import FileResponse
 
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 load_dotenv()
 
 API_KEY_CREDITS = {os.getenv("API_KEY"): 5}
@@ -21,3 +25,24 @@ def generate(prompt: str, x_api_key: str = Depends(verify_api_key)):
     API_KEY_CREDITS[x_api_key] -= 1
     response = ollama.chat(model="starcoder2:3b", messages=[{"role": "user", "content": prompt}])
     return {"response": response["message"]["content"]}
+
+
+@app.get("/download/{filename}")
+async def download_file(filename: str):
+    """Endpoint para baixar arquivos da pasta uploads"""
+    filename.replace("%", " ")
+    print("Download filename: ", filename)
+
+    file_path = os.path.join(UPLOAD_DIR, filename)
+
+    if os.path.exists(file_path):
+        return FileResponse(file_path, media_type="application/octet-stream", filename=filename)
+    return {"error": "Arquivo não encontrado"}
+
+# if __name__ == "__main__":
+#     import uvicorn
+
+#     port = int(os.getenv("SERVER_PORT", 3000))
+#     host = os.getenv("SERVER_HOST", "0.0.0.0")
+
+#     uvicorn.run(app, host=host, port=port)
--- api/src/test_api.py
+++ api/src/test_api.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-url = "http://127.0.0.1:8000/generate?prompt=Tell me about Python"
+url = "https://ollama.monynha.me/generate?prompt=Tell me about Python"
 headers = {"x-api-key": os.getenv("API_KEY"), "Content-Type": "application/json"}
 
 response = requests.post(url, headers=headers)
